00f6430683e19a03e3ae79b6d40da2f1d35166fd
DESKTOP-JARMAPR\HEKA <6jo.oneroom@gmail.com>
2021-03-14 12:55:23 +0900

    中村作業分(ログイン機能/サインアップ機能/サインアップ確認）

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,68 @@
-import React from 'react'
+//Login.js
+import React, { useState } from 'react'
+import firebase from '../config/firebase'
+import { AuthContext } from '../AuthService'
+import { Redirect, Link } from 'react-router-dom'
 
-const Login = () => {
-    return (
-        <h1>Login</h1>
-    )
+const Login = ({ history }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  //とりあえずパスとメールだけで一旦作る
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push("/")
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+
+
+  const inputEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const inputPassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  // const user = useContext(AuthContext)
+  // if (user) {
+  //   return <Redirect to="/" />
+  // }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='Email'
+            onChange={inputEmail}
+            />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='Password'
+            onChange={inputPassword}
+          />
+        </div>
+        {/* <button type='submit'>ログイン</button> */}
+        <Link to="/signup">新規登録へ</Link>
+      </form>
+    </>
+  )
 }
 
 export default Login
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,49 @@
-import React from 'react'
+//SignUp.js
+import React, { useState } from 'react'
+import firebase from '../config/firebase'
+import { useHistory, Link } from 'react-router-dom'
+import { useForm, FormProvider } from 'react-hook-form';
+import SignUpResult from './SignUpResult'
 
 const SignUp = () => {
-    return (
-        <h1>SignUp</h1>
-    )
+  const history = useHistory()
+  const { register, handleSubmit, errors, watch, reset, getValues } = useForm()
+  const [userData, setUserData] = useState()
+
+  const onSubmit = data => {
+    // console.log(errors);
+    history.push({ pathname: './signupresult', state: userData })
+  }
+
+  const onChange = () => {
+    setUserData(getValues)
+  }
+
+
+//必要そうな登録内容
+  //★とりあえず実装するやつ
+  //ユーザー名（あとから変更可能）
+  //email（あとから変更可能）
+  //password（あとから変更可能）
+
+  //★あとから実装するやつ
+  //性別
+  //住んでるエリア（あとから変更可能）
+  return (
+    <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="name">ユーザー名</label>
+          <input type="text" placeholder="ユーザー名" name="username" onChange={onChange} ref={register} />
+          <label htmlFor="email">メールアドレス</label>
+          <input type="text" placeholder="Email" name="email" onChange={onChange} ref={register} />
+          <label htmlFor="password">パスワード</label>
+          <input type="text" placeholder="パスワード" name="password" onChange={onChange} ref={register} />
+        <button type="submit">確認画面へ</button>
+        <Link to="/login">ログインへ</Link>
+      </form>
+      </>
+  )
 }
+
 
 export default SignUp
