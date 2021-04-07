0f8bba72e62718be43a917b4ebb53efb7c5e51cb
Na0yA1031 <kamekameriki@gmail.com>
2021-04-07 21:10:04 +0900

    メイン画面の作成とユーザー画面の作成と修正

--- src/components/UserInfo/OtherUserInfo.js
+++ src/components/UserInfo/OtherUserInfo.js
@@ -1,60 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import FormDialog from '../Forms/FormDialog'
 import FavoriteShop from './FavoriteShop';
 import ImageArea from './ImageArea';
-import EditIcon from '@material-ui/icons/Edit';
-import { makeStyles, TextField } from '@material-ui/core';
-import Button from '@material-ui/core/Button'
-import { AuthContext } from '../../AuthProvider';
+import { TextField } from '@material-ui/core';
 
-const useStyles = makeStyles({
-    'button': {
-        '@media screen and (max-width: 600px)': {
-            marginTop: '30px'
-        }
-    }
-})
 
-const UserInfo = () => {
+
+const OtherUserInfo = () => {
     // プロフィール編集用のモーダルの開閉を管理するstate
     const [open, setOpen] = useState(false);
 
     // 画面上にある自己紹介の部分に表示させる文字列を代入するステート
     const [selfIntroduction, setSelfIntroduction] = useState('')
 
-    // フォトアイコンクリック→画像選択後に画像情報が入ってくるステート
-    const [images, setImages] = useState('');
-
-    // プロフィール編集ボタンをクリックしたらstate:openがtrueになる
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
     // 特定の条件(編集の確定やキャンセル、背景をクリック)したときにモーダルを閉じる関数
     const handleClose = () => {
         setOpen(false);
     };
 
-    const classes = useStyles();
-
-    const { authUser } = useContext(AuthContext)
-    console.log(authUser)
-
-    const disabled = authUser ? false : true;
-
     return (
         <>
             <div className='user-profile container'>
                 <div className='user-profile-config'>
-                    <ImageArea images={images} />
-                    <div>
-
-                        <Button className={classes.button} onClick={handleClickOpen} endIcon={<EditIcon />} color={'primary'} variant={'contained'} disabled={disabled} >プロフィールを編集</Button>
-                    </div>
+                    <ImageArea />
+                    {/* <Button className={classes.button} onClick={handleClickOpen} endIcon={<EditIcon />} color={'primary'} variant={'contained'}>プロフィールを編集</Button> */}
+                    {/* <button className='btn' onClick={handleClickOpen}>プロフィールを編集</button> */}
                 </div>
 
-                <div className='user-info-wrap'>
-                    <h2 className="user-info-title user-info-title-first ">ユーザーネーム</h2>
+                <div className='user-info-wrap user-info-wrap-first'>
+                    <h2 className="user-info-title user-info-title-first ">N.F</h2>
                     {/* <p className='user-info-desc'>自己紹介</p> */}
                     {/* <p style={{ marginTop: '7px', lineHeight: '1.65', wordWrap: 'break-word' }}>
                         {selfIntroduction}
@@ -95,11 +69,9 @@ const UserInfo = () => {
                 open={open}
                 handleClose={handleClose}
                 setSelfIntroduction={setSelfIntroduction}
-                images={images}
-                setImages={setImages}
             />
         </>
     )
 }
 
-export default UserInfo;
+export default OtherUserInfo;
